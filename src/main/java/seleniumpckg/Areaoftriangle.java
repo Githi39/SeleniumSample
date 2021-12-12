5c43e464a001a33efce759544b34f95e5015af42
Githi39 <githi39@gmail.com>
2021-12-12 11:54:54 +0530

    initial comit

---
+++ src/main/java/seleniumpckg/Areaoftriangle.java
@@ -0,0 +1,18 @@
+package seleniumpckg;
+
+public class Areaoftriangle {
+
+
+    public static void main(String args[])
+    {
+        double b = 40;
+        System.out.println("width of the Triangle:" + b);
+
+        double h = 150;
+        System.out.println("height of the Triangle:" + h);
+
+
+        double area = (b * h) / 2;
+        System.out.println("Area of Triangle is: " + area);
+    }
+}
